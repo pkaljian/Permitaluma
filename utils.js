58c45d981270e7b85b8c7ade70edc3e4313018b6
pkaljian <pkaljian@gmail.com>
2025-10-16 16:15:28 -0700

    Update utils.js
    added lines 9-12
    export function qp(name, url = window.location.href) {
      const u = new URL(url);
      return u.searchParams.get(name);
    }

--- utils.js
+++ utils.js
@@ -6,3 +6,7 @@ export function linkTo(el, url) {
   el.addEventListener('click', () => window.location.href = url);
   el.classList.add('clickable');
 }
+export function qp(name, url = window.location.href) {
+  const u = new URL(url);
+  return u.searchParams.get(name);
+}
